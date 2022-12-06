aabac97f6f2159f6d9e7c3dcb320d80831254005
JessicaMoreschi <71377205+JessicaMoreschi@users.noreply.github.com>
2022-12-06 10:06:49 +0100

    layout products (cards)

--- script.js
+++ script.js
@@ -1,141 +1,197 @@
 // Interpret the data as the correct format. Use d3.csv or d3.tsv accordingly.
-data = d3.csv("data/prov.csv");
+data = d3.csv("assets/data/prov.csv");
 
 // Select the container where we will put our HTML elements
 let container = d3.select(".container");
 
 container.append('div')
     .attr('id', 'all-filters')
-    .attr('class', 'row no-gutters')
-    .style('column-gap', '20px')
+    .attr('class', 'flex-row')
 
 container.append('div')
     .attr('id', 'all-cards')
     .attr('class', 'card-columns')
 
+
 let cards = d3.select("#all-cards")
 
 
-
 // Load data
-
-
 data.then(function (data) {
     // Do stuff with your data!
     // Join your data to the creation of div elements to the same number of items in your dataset.
     let card = cards.selectAll("div")
         .data(data)
         .join("div")
-        .attr("class", "card")
+        .attr("class", "flex-column card")
 
     // We append to the initial card all the parts required.
     // HEADER
-
     card.append("div")
-        .attr("class", "card-body")
-        .append('p')
-        .attr("class", "card-text")
+        .attr("class", "flex-column cardHeader")
+        .append('img')
+        .attr('src', 'assets/icon/link.svg')
+
+    card.append("p")
+        .attr("class", "flex-row card-content")
         .text(function (data) {
             return data.Context
         })
 
     card.append("div")
-        .attr("class", "card-footer")
-        .append('ul')
-        .attr("class", "nav nav-pills card-header-pills")
-        .append('li')
-        .attr("class", "nav-item")
-        .append('small')
-        .attr("class", "text-muted")
-        .text(function (d) {
-            let topic = [];
-            if (d.TagAnimal == 'TRUE') {
-                topic.push('>Animal')
-            }
-            if (d.TagDistribution == 'TRUE') {
-                topic.push('>distribution')
-            }
-            if (d.TagEnergy == 'TRUE') {
-                topic.push('>energy')
-            }
-            if (d.TagFood == 'TRUE') {
-                topic.push('>food')
-            }
-            if (d.TagManagement == 'TRUE') {
-                topic.push('>management')
-            }
-            if (d.TagWaste == 'TRUE') {
-                topic.push('>waste')
-            }
-            if (d.ToV == 'TRUE') {
-                topic.push('data')
-            } else if (d.ToV == 'FALSE') {
-                topic.push('noData')
-            }
-            if (d.Actions == 'TRUE') {
-                topic.push('action')
-            } else if (d.Actions == 'FALSE') {
-                topic.push('noAction')
-            }
-            return d.SentenceAim + " " + d.Keyword + " " + topic.join(' ');
+        .attr("class", "flex-row cardFooter")
 
+
+        .append('div')
+        .attr("class", "flex-row roundTag")
+    var tagk = card.select('.roundTag', ":last-child")
+        .style('background-color', "var(--primary-color)")
+    tagk.append('img')
+        .attr('src', 'assets/icon/keyword.svg')
+    tagk.append("p")
+        .text(function (d) {
+            return d.Keyword
+        })
+
+
+    card.select(".cardFooter")
+        .append('div')
+        .attr("class", "flex-row roundTag T1")
+    var tagT1 = card.select('.T1')
+        .style('display', function (d) {
+            if (d.TagAnimal != 'TRUE') {
+                return 'none'
+            } else { return 'flex' }
+        })
+    tagT1.append('img')
+        .attr('src', 'assets/icon/topic.svg')
+    tagT1.append("p")
+        .text(function (d) {
+            if (d.TagAnimal == 'TRUE') {
+                return 'Animal'
+            }
+        })
+
+    card.select(".cardFooter")
+        .append('div')
+        .attr("class", "flex-row roundTag T2")
+    var tagT2 = card.select('.T2')
+        .style('display', function (d) {
+            if (d.TagDistribution != 'TRUE') {
+                return 'none'
+            } else { return 'flex' }
+        })
+    tagT2.append('img')
+        .attr('src', 'assets/icon/topic.svg')
+    tagT2.append("p")
+        .text(function (d) {
+            if (d.TagDistribution == 'TRUE') {
+                return 'distribution'
+            }
+        })
+
+    card.select(".cardFooter")
+        .append('div')
+        .attr("class", "flex-row roundTag T3")
+    var tagT3 = card.select('.T3')
+        .style('display', function (d) {
+            if (d.TagEnergy != 'TRUE') {
+                return 'none'
+            } else { return 'flex' }
+        })
+    tagT3.append('img')
+        .attr('src', 'assets/icon/topic.svg')
+    tagT3.append("p")
+        .text(function (d) {
+            if (d.TagEnergy == 'TRUE') {
+                return 'energy'
+            }
+        })
+
+    card.select(".cardFooter")
+        .append('div')
+        .attr("class", "flex-row roundTag T4")
+    var tagT4 = card.select('.T4')
+        .style('display', function (d) {
+            if (d.TagFood != 'TRUE') {
+                return 'none'
+            } else { return 'flex' }
+        })
+    tagT4.append('img')
+        .attr('src', 'assets/icon/topic.svg')
+    tagT4.append("p")
+        .text(function (d) {
+            if (d.TagFood == 'TRUE') {
+                return 'food'
+            }
+        })
+
+    card.select(".cardFooter")
+        .append('div')
+        .attr("class", "flex-row roundTag T5")
+    var tagT5 = card.select('.T5')
+        .style('display', function (d) {
+            if (d.TagManagement != 'TRUE') {
+                return 'none'
+            } else { return 'flex' }
+        })
+    tagT5.append('img')
+        .attr('src', 'assets/icon/topic.svg')
+    tagT5.append("p")
+        .text(function (d) {
+            if (d.TagManagement == 'TRUE') {
+                return 'management'
+            }
+        })
+
+    card.select(".cardFooter")
+        .append('div')
+        .attr("class", "flex-row roundTag T6")
+    var tagT6 = card.select('.T6')
+        .style('display', function (d) {
+            if (d.TagWaste != 'TRUE') {
+                return 'none'
+            } else { return 'flex' }
+        })
+    tagT6.append('img')
+        .attr('src', 'assets/icon/topic.svg')
+    tagT6.append("p")
+        .text(function (d) {
+            if (d.TagWaste == 'TRUE') {
+                return 'waste'
+            }
+        })
+
+    card.select(".cardFooter")
+        .append('div')
+        .attr("class", "flex-row squareTag Data")
+    var tagData = card.select('.Data')
+    tagData.append('img')
+        .attr('src', 'assets/icon/data.svg')
+    tagData.append("p")
+        .text(function (d) {
+            if (d.Data == 'TRUE') {
+                return 'data'
+            } else {
+                return 'no data'
+            }
+        })
+
+    card.select(".cardFooter")
+        .append('div')
+        .attr("class", "flex-row squareTag Act")
+    var tagAct = card.select('.Act')
+    tagAct.append('img')
+        .attr('src', 'assets/icon/action.svg')
+    tagAct.append("p")
+        .text(function (d) {
+            if (d.Actions == 'TRUE') {
+                return 'take action'
+            } else {
+                return 'dissemination'
+            }
         })
 })
-
-const recipes = [
-    {
-        recipeId: 0,
-        SentenceAim: undefined,
-        Keyword: undefined,
-        ToV: undefined,
-        Actions: undefined,
-        TagAnimal: undefined,
-        TagDistribution: undefined,
-        TagEnergy: undefined,
-        TagFood: undefined,
-        TagManagement: undefined,
-        TagWaste: undefined,
-    },
-    {
-        recipeId: 1,
-        SentenceAim: 'take action',
-        Keyword: 'impact',
-        ToV: 'TRUE',
-        Actions: 'TRUE',
-        TagAnimal: undefined,
-        TagDistribution: undefined,
-        TagEnergy: undefined,
-        TagFood: undefined,
-        TagManagement: undefined,
-        TagWaste: undefined,
-    },
-    {
-        recipeId: 2,
-        SentenceAim: 'dissemination',
-        Keyword: 'recycle',
-        ToV: 'TRUE',
-        Actions: 'FALSE',
-        TagAnimal: undefined,
-        TagDistribution: undefined,
-        TagEnergy: undefined,
-        TagFood: undefined,
-        TagManagement: undefined,
-        TagWaste: undefined,
-    },
-    {
-        recipeId: 3,
-        SentenceAim: 'take action',
-        Keyword: 'greenhouse gas',
-        ToV: 'FALSE',
-        Actions: 'FALSE',
-        TagAnimal: undefined,
-        TagDistribution: undefined,
-        TagEnergy: undefined,
-        TagFood: undefined,
-        TagManagement: undefined,
-        TagWaste: undefined,
-    }
-]
 
 //filter
 let filterDiv = d3.select("#all-filters")
@@ -143,22 +199,25 @@ let filter = filterDiv.selectAll("div")
     .data(recipes)
     .join("div")
     .attr("class", "card text-white bg-primary mb-3")
-    .style("max-width","10rem")
+    .style("max-width", "10rem")
 filter.append("div")
     .attr("class", "card-body")
     .append('p')
     .attr("class", "card-text")
     .text(function (a) {
-        return JSON.stringify(a, null, 4)})
+        return JSON.stringify(a, null, 4)
+    })
 filter.append("div")
-    .attr("class", "card-footer")
+    .attr("class", "cardFooter")
     .append("button")
     .attr("type", "button")
     .attr("class", "btn btn-light btn-sm")
     .attr("name", function (a) {
-        return a.recipeId})
+        return a.recipeId
+    })
     .text(function (a) {
-        return 'recipe' + a.recipeId})
+        return 'recipe' + a.recipeId
+    })
     .on('click', function (btnId) {
         let recipe = recipes[btnId.srcElement.name];
         d3.select("#all-cards").selectAll('.card')
@@ -167,6 +226,7 @@ filter.append("div")
                 return (
                     ((d.SentenceAim == recipe.SentenceAim) || (recipe.SentenceAim == undefined)) &&
                     ((d.Keyword == recipe.Keyword) || (recipe.Keyword == undefined)) &&
+                    ((d.ToV == recipe.ToV) || (recipe.ToV == undefined)) &&
                     ((d.Actions == recipe.Actions) || (recipe.Actions == undefined)) &&
                     ((d.TagAnimal == recipe.TagAnimal) || (recipe.TagAnimal == undefined)) &&
                     ((d.TagDistribution == recipe.TagDistribution) || (recipe.TagDistribution == undefined)) &&
